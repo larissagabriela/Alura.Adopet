2977ee5ea954d246d14c62a441acbe98076d827d
laris <larissagabriela@ufrj.br>
2023-04-12 09:51:52 -0300

    #Projeto inicial do curso

--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -54,7 +54,8 @@ try
                 Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
                 Console.WriteLine("Comando possíveis: ");
                 Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
-                Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
+                Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado.");
+                Console.WriteLine($" adopet list   comando que exibe no terminal o conteúdo cadastrado na base de dados da AdoPet." + "\n\n\n\n");
                 Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
             }
             // exibe o help daquele comando específico
@@ -69,6 +70,11 @@ try
                 {
                     Console.WriteLine(" adopet show <arquivo>  comando que " +
                         "exibe no terminal o conteúdo do arquivo importado.");
+                }
+                if (args[1].Equals("list"))
+                {
+                    Console.WriteLine($" adopet list   " +
+                        $"comando que exibe no terminal o conteúdo cadastrado na base de dados da AdoPet.");
                 }
             }
             break;
