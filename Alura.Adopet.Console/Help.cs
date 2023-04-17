f461f6b990c72b45f0694d721ce2aefd1af030cb
laris <larissagabriela@ufrj.br>
2023-04-17 11:34:06 -0300

    #DESAFIO Extração de classes [List e Help]

---
+++ Alura.Adopet.Console/Help.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Console
+{
+    internal class Help
+    {
+        public void Documentacao()
+        {
+            System.Console.WriteLine("Lista de comandos.");
+            System.Console.WriteLine("adopet help <parametro> ous simplemente adopet help  " +
+                    "comando que exibe informações de ajuda dos comandos.");
+            System.Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
+            System.Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
+            System.Console.WriteLine("Comando possíveis: ");
+            System.Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
+            System.Console.WriteLine("adopet list  comando que exibe no terminal o conteúdo da base de dados da AdoPet.");
+            System.Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
+            System.Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
+
+        }
+
+        public void HelpDoComando(string comando)
+        {
+            if (comando.Equals("import"))
+            {
+                System.Console.WriteLine(" adopet import <arquivo> " +
+                    "comando que realiza a importação do arquivo de pets.");
+            }
+            if (comando.Equals("show"))
+            {
+                System.Console.WriteLine(" adopet show <arquivo>  comando que " +
+                    "exibe no terminal o conteúdo do arquivo importado.");
+            }
+            if (comando.Equals("list"))
+            {
+                System.Console.WriteLine(" adopet list  comando que " +
+                    "exibe no terminal o conteúdo da base de dados da AdoPet.");
+            }
+        }
+    }
+}
--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -2,8 +2,6 @@
 using System.Net.Http.Json;
 using Alura.Adopet.Console;
 
-// cria instância de HttpClient para consumir API Adopet
-HttpClient client = ConfiguraHttpClient("http://localhost:5057");
 Console.ForegroundColor = ConsoleColor.Green;
 try
 {
@@ -15,13 +13,14 @@ try
             await import.ImportarPetsAsync(caminhoDoArquivo: args[1]);
             break;
         case "help":
+            var help = new Help();
             if (args.Length == 2)
             {
-                HelpDoComando(comando: args[1]);
+                help.HelpDoComando(comando: args[1]);
             }
             else
             {
-                Help();
+                help.Documentacao();
             }
             break;
         case "show":
@@ -29,7 +28,8 @@ try
             show.MostrarArquivo(caminhoDoArquivo: args[1]);
             break;
         case "list":
-            await ListAsync(client);
+            var list = new List();
+            await list.ListarPetsAsync();
             break;
         default:
             // exibe mensagem de comando inválido
@@ -46,72 +46,4 @@ catch (Exception ex)
 finally
 {
     Console.ForegroundColor = ConsoleColor.White;
-}
-
-HttpClient ConfiguraHttpClient(string url)
-{
-    HttpClient _client = new HttpClient();
-    _client.DefaultRequestHeaders.Accept.Clear();
-    _client.DefaultRequestHeaders.Accept.Add(
-        new MediaTypeWithQualityHeaderValue("application/json"));
-    _client.BaseAddress = new Uri(url);
-    return _client;
-}
-
-Task<HttpResponseMessage> CreatePetAsync(Pet pet)
-{
-    HttpResponseMessage? response = null;
-    using (response = new HttpResponseMessage())
-    {
-        return client.PostAsJsonAsync("pet/add", pet);
-    }
-}
-
-async Task<IEnumerable<Pet>?> ListPetsAsync()
-{
-    HttpResponseMessage response = await client.GetAsync("pet/list");
-    return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
-}
-
-void Help()
-{
-    Console.WriteLine("Lista de comandos.");
-    Console.WriteLine("adopet help <parametro> ous simplemente adopet help  " +
-            "comando que exibe informações de ajuda dos comandos.");
-    Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
-    Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
-    Console.WriteLine("Comando possíveis: ");
-    Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
-    Console.WriteLine("adopet list  comando que exibe no terminal o conteúdo da base de dados da AdoPet.");
-    Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
-    Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
-
-}
-
-void HelpDoComando(string comando)
-{
-    if (comando.Equals("import"))
-    {
-        Console.WriteLine(" adopet import <arquivo> " +
-            "comando que realiza a importação do arquivo de pets.");
-    }
-    if (comando.Equals("show"))
-    {
-        Console.WriteLine(" adopet show <arquivo>  comando que " +
-            "exibe no terminal o conteúdo do arquivo importado.");
-    }
-    if (comando.Equals("list"))
-    {
-        Console.WriteLine(" adopet list  comando que " +
-            "exibe no terminal o conteúdo da base de dados da AdoPet.");
-    }
-}
-
-async Task ListAsync(HttpClient client)
-{
-    var pets = await ListPetsAsync();
-    foreach (var pet in pets)
-    {
-        Console.WriteLine(pet);
-    }
 }
